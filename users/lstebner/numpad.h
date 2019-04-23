c22d3da4c3298fc411bd4037a178db00a5434f76
lstebner <luke.stebner@gmail.com>
2019-04-22 22:33:44 -0700

    adding users/lstebner

---
+++ users/lstebner/numpad.h
@@ -0,0 +1,14 @@
+#pragma once
+
+enum numpad_layers {
+  LAYER_NUMPAD,
+  LAYER_SYMBOLS,
+};
+
+#define NUMPAD_ROW_3 KC_7,     KC_8,     KC_9,     KC_KP_PLUS
+#define NUMPAD_ROW_2 KC_4,     KC_5,     KC_6,     KC_KP_MINUS
+#define NUMPAD_ROW_1  KC_1,     KC_2,     KC_3,     KC_BSPACE
+#define NUMPAD_ROW_0 LT(LAYER_SYMBOLS, KC_KP_DOT) ,KC_0,     KC_LEFT,  KC_RIGHT 
+
+#define LAYOUT_ortho_4x4_wrapper(...) LAYOUT_ortho_4x4(__VA_ARGS__)
+
