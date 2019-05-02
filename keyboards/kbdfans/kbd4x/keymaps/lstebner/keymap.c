83e8b2791de1ace949d88f672922c4e45d8f4623
lstebner <luke.stebner@gmail.com>
2019-05-01 20:31:51 -0700

    <wip> niu layout

--- keyboards/kbdfans/kbd4x/keymaps/lstebner/keymap.c
+++ keyboards/kbdfans/kbd4x/keymaps/lstebner/keymap.c
@@ -14,33 +14,44 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "keys.h"
+#include "lstebner.h"
 
 // Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-  KBD4X = SAFE_RANGE,
+enum niu_layers {
+  LAYER_DEFAULT,
+  LAYER_DEFAULT_ALT,
+  LAYER_NUMROW,
+  LAYER_MACRO,
+  LAYER_SYMBOLS,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_niu_wrapper (
+  [LAYER_DEFAULT] = LAYOUT_niu_wrapper (
     KC_GESC,        KG_QWERTYUIOP,  KC_BSPC,
     LT(2, KC_TAB),  KG_ASDFGHJKL,    KC_SCLN, KC_QUOT,
     KC_LSHIFT,      KG_ZXCVBNM,    KC_COMM, KC_DOT,  KC_SLSH,   KC_GRAVE,
-    CTRL, ALT, CMD,    KC_2,    OSL(1),    KC_UNDERSCORE, KC_SPACE,   MO(1),   KC_SLSH, KC_LBRC, KC_RBRC, KC_ENTER
+    CTRL, ALT, CMD,   KC_2,    OSL(1),    KC_UNDERSCORE, KC_SPACE,   MO(1),   KC_SLSH, KC_LBRC, KC_RBRC, KC_ENTER
   ),
 
-  [1] = LAYOUT_niu_wrapper (
-    KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
+  [LAYER_NUMROW] = LAYOUT_niu_wrapper (
+    KC_GRAVE, KG_NUMBER_KEYS, KC_MINS,
     ___,      RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, ___,  ___,  ___,  ___,  ___,  ___,  KC_EQL,
     ___,      BL_TOGG, BL_STEP, ___,     ___,     ___,  ___,  ___,  ___,  ___,  ___,  ___,
-    ___,      ___,     ___,     ___,     ___,     ___,  ___,  ___,  ___,  ___,  ___,  RESET
+    BLANK11,  RESET
   ),
 
-  [2] = LAYOUT_niu_wrapper (
+  [LAYER_DEFAULT_ALT] = LAYOUT_niu_wrapper (
     ___,  ___,   ___,  ___,  ___, ___,  ___,  ___,  KC_UP,  ___,  ___,  ___,
     ___,  ___,   ___,  ___,  ___,     ___,  ___, KC_LEFT,  KC_DOWN,  KC_RIGHT,  ___,  ___,
-    ___,  ___,   ___,  ___,  ___,     ___,  ___,  ___,  ___,  ___,  ___,  ___,
+    ___,  ___,   ___,  ___,  ___,     ___,  ___,  ___,  ___,  ___,  ___,  KC_CAPS,
     ___,  ___,   ___,  ___,  ___,     ___,  ___,  ___,  ___,  ___,  ___,  ___
+  ),
+
+  [LAYER_SYMBOLS] = LAYOUT_niu_wrapper (
+    BLANK12,
+    BLANK12,
+    BLANK12,
+    BLANK12
   ),
 };
 
