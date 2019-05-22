88ed8ca67e8e06c2d17b7b6dfa16dd8bcbdbbbd4
lstebner <luke.stebner@gmail.com>
2019-05-21 21:37:50 -0700

    reducing iris firmware size with rules; and moved a quote on MO layer

--- keyboards/keebio/iris/keymaps/lstebner/keymap.c
+++ keyboards/keebio/iris/keymaps/lstebner/keymap.c
@@ -15,35 +15,36 @@ enum custom_keycodes {
 };
 
 #define BASE_ALT  MO(_BASE_ALT)
-#define VIM       OSL(_VIM)
+#define VIM_OSL   OSL(_VIM)
+#define CMDSHIFT  LGUI(KC_LSFT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      BASE_ALT,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,          VIM,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LSFT,          VIM_OSL, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LCTRL,KC_LGUI,  KC_ENT,                    KC_SPC, KC_UNDS, KC_RALT
+                                    KC_LCTRL,KC_LGUI,  KC_ENT,                   KC_SPC,  KC_UNDS, KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_BASE_ALT] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PLUS,
+     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQUAL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, QUIT,    CLOSE_WINDOW,_______,RELOAD,NEW_TAB,                           _______, _______, KC_UP,   KC_LBRC, KC_RBRC, KC_PIPE,
+     _______, QUIT,    CLOSE_WINDOW,___, RELOAD,  NEW_TAB,                            KC_PLUS, _______, KC_UP,   KC_LBRC, KC_RBRC, KC_PIPE,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, SELECT_ALL,SAVE,  DROP,    FIND,    FIND_NEXT,                          _______, KC_LEFT, KC_DOWN, KC_RIGHT,KC_GRV,  KC_MINS,
+     _______, SELECT_ALL,SAVE,  DROP,    FIND,    FIND_NEXT,                          KC_MINS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_GRV,  KC_DQUO,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, UNDO,    CUT,     COPY,    PASTE,   _______, _______,          KC_CAPS, _______, _______, VIM_PREV_TAB, VIM_NEXT_TAB, _______, _______,
+     _______, UNDO,    CUT,     COPY,    PASTE,   _______, _______,          KC_CAPS, _______, _______, KC_LCBR, KC_RCBR, ___,     ___,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    VIM_PREV_TAB,VIM_NEXT_TAB,KC_ENT,                  KC_UNDS, PREV_TAB,NEXT_TAB 
+                            VIM_PREV_TAB, VIM_NEXT_TAB, ___,                    KC_UNDS, PREV_TAB,NEXT_TAB 
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
