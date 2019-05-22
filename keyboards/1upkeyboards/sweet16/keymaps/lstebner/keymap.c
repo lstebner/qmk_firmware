d827fbf1e986c86e9e993935b02b372c6089dd7e
lstebner <luke.stebner@gmail.com>
2019-05-21 21:37:27 -0700

    turned sweet16 into emojipad

--- keyboards/1upkeyboards/sweet16/keymaps/lstebner/keymap.c
+++ keyboards/1upkeyboards/sweet16/keymaps/lstebner/keymap.c
@@ -4,37 +4,132 @@
 enum custom_keycodes {
   FN = SAFE_RANGE,
   MAKE_CMD,
+  EMOJI_JOY, //
+  EMOJI_SMILE, //
+  EMOJI_TEAR, //
+  EMOJI_RAISED_HANDS, //
+  EMOJI_SPARKLE_THUMB, //
+  EMOJI_PEACE, //
+  EMOJI_FIRE, //
+  EMOJI_INFO_DESK, //
+  EMOJI_THUMBS_UP, //
+  EMOJI_THUMBS_DOWN, //
+  EMOJI_CRY_HAPPY, //
+  EMOJI_DANGO, //
+  EMOJI_TACO,
+  SAC, //
 };
+
+#define LAYER_EMOJI        0
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // DEFAULT LAYER
     // -------------------------------------
-    // |  KP 7  |  KP 8  |  KP 9  |        |
-    // ---------------------------|  KP +  |
-    // |  KP 4  |  KP 5  |  KP 6  |        |
+    // | smile  |  joy   |  tear  |        |
+    // ---------------------------|  plus  |
+    // |raisedhands|infodesk|sparklethumb|        |
     // -------------------------------------
-    // |  KP 1  |  KP 2  |  KP 3  |        |
+    // |  -1    |  peace |  dango  |        |
     // ---------------------------| ENTER  |
-    // |  KP 0  |  KP .  | NUMLOCK|        |
+    // |  fire  |  cryhappy| SAC    |        |
     // -------------------------------------
-    LAYOUT_ortho_4x4(
-        KC_KP_7, KC_KP_8,   KC_KP_9,   KC_KP_PLUS,
-        KC_KP_4, KC_KP_5,   KC_KP_6,   xxx,
-        KC_KP_1, KC_KP_2,   KC_KP_3,   KC_ENTER,
-        KC_KP_0, KC_KP_DOT, KC_NUMLOCK, xxx
-    )
+    [LAYER_EMOJI] = LAYOUT_ortho_4x4(
+        EMOJI_SMILE, EMOJI_JOY,   EMOJI_TEAR,   KC_KP_PLUS,
+        EMOJI_RAISED_HANDS, EMOJI_INFO_DESK,   EMOJI_SPARKLE_THUMB,   xxx,
+        EMOJI_THUMBS_DOWN, EMOJI_PEACE,   EMOJI_FIRE,   KC_ENTER,
+        EMOJI_THUMBS_UP, EMOJI_CRY_HAPPY, SAC, xxx
+    ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case FN:
-            if (record->event.pressed) {
-              // todo
-            }
-            break;
         case MAKE_CMD:
             if (record->event.pressed) {
               SEND_STRING("make 1upkeyboards/sweet16:lstebner");
+            }
+            break;
+
+        case EMOJI_JOY:
+            if (record->event.pressed) {
+              SEND_STRING(":joy:");
+            }
+            break;
+
+        case EMOJI_SMILE:
+            if (record->event.pressed) {
+              SEND_STRING(":smile:");
+            }
+            break;
+
+        case EMOJI_TEAR:
+            if (record->event.pressed) {
+              SEND_STRING(":cry:");
+            }
+            break;
+
+        case EMOJI_RAISED_HANDS:
+            if (record->event.pressed) {
+              SEND_STRING(":raised_hands:");
+            }
+            break;
+
+        case EMOJI_SPARKLE_THUMB:
+            if (record->event.pressed) {
+              SEND_STRING(":sparklethumb:");
+            }
+            break;
+
+        case EMOJI_PEACE:
+            if (record->event.pressed) {
+              SEND_STRING(":peace:");
+            }
+            break;
+
+        case EMOJI_FIRE:
+            if (record->event.pressed) {
+              SEND_STRING(":fire:");
+            }
+            break;
+
+        case EMOJI_INFO_DESK:
+            if (record->event.pressed) {
+              SEND_STRING(":information_desk_person:");
+            }
+            break;
+
+        case EMOJI_THUMBS_UP:
+            if (record->event.pressed) {
+              SEND_STRING(":+1:");
+            }
+            break;
+
+        case EMOJI_THUMBS_DOWN:
+            if (record->event.pressed) {
+              SEND_STRING(":-1:");
+            }
+            break;
+
+        case EMOJI_CRY_HAPPY:
+            if (record->event.pressed) {
+              SEND_STRING(":cry_happy:");
+            }
+            break;
+
+        case EMOJI_DANGO:
+            if (record->event.pressed) {
+              SEND_STRING(":dango:");
+            }
+            break;
+
+        case EMOJI_TACO:
+            if (record->event.pressed) {
+              SEND_STRING(":taco:");
+            }
+            break;
+
+        case SAC:
+            if (record->event.pressed) {
+              SEND_STRING("Streamline Account Creation");
             }
             break;
     }
