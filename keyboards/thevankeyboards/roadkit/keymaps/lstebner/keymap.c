59eeb3774a6e1d23025e878e13cec8fc05d26ea0
lstebner <luke.stebner@gmail.com>
2019-05-23 10:09:30 -0700

    roadkit emojinumpad

--- keyboards/thevankeyboards/roadkit/keymaps/lstebner/keymap.c
+++ keyboards/thevankeyboards/roadkit/keymaps/lstebner/keymap.c
@@ -1,5 +1,4 @@
 #include QMK_KEYBOARD_H
-#include "numpad.h"
 
 extern keymap_config_t keymap_config;
 
@@ -9,35 +8,48 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 
 enum custom_keycodes {
-  NEW_TAB,
-  NEW_WINDOW,
-  PREV_TAB,
-  NEXT_TAB,
-  COPY,
-  PASTE,
-  NEXT_APP,
-  PREV_APP,
-  CHANGE_WINDOW,
+  MAKE_CMD = SAFE_RANGE,
+  EMOJI_JOY, //
+  EMOJI_SMILE, //
+  EMOJI_TEAR, //
+  EMOJI_RAISED_HANDS, //
+  EMOJI_SPARKLE_THUMB, //
+  EMOJI_PEACE, //
+  EMOJI_FIRE, //
+  EMOJI_INFO_DESK, //
+  EMOJI_THUMBS_UP, //
+  EMOJI_THUMBS_DOWN, //
+  EMOJI_CRY_HAPPY, //
+  EMOJI_DANGO, //
+  EMOJI_TACO,
+  EMOJI_COOLGHOST,
+  EMOJI_PLUSONE,
 };
+
+#define LAYER_NUMPAD  0
+#define LAYER_EMOJI   1
+
+#define _NUMPAD MO(LAYER_NUMPAD)
+#define _EMOJI MO(LAYER_EMOJI)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // NUMPAD
   // 
   // -------------------------
-  // |  7  |  8  |  9  |  +  |
+  // |  7  |  8  |  9  | BACK|
   // -------------------------
-  // |  4  |  5  |  6  |  -  |
+  // |  4  |  5  |  6  |  +  |
   // -------------------------
-  // |  1  |  2  |  3  |BACK |
+  // |  1  |  2  |  3  |  -  |
   // -------------------------
-  // |  .  |  0  |LEFT |RIGHT|
+  // |  .  |  0  |LAYER|ENTER|
   // -------------------------
-  [LAYER_NUMPAD] = LAYOUT_ortho_4x4_wrapper(
-    NUMPAD_ROW_3, \
-    NUMPAD_ROW_2, \
-    NUMPAD_ROW_1, \
-    NUMPAD_ROW_0
+  [LAYER_NUMPAD] = LAYOUT_ortho_4x4(
+      KC_KP_7, KC_KP_8, KC_KP_9, KC_BSPACE,
+      KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_PLUS,
+      KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_MINUS,
+      KC_KP_DOT, KC_KP_0, _EMOJI, KC_ENTER
   ),
 
   // SYMBOLS
@@ -51,71 +63,112 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // -------------------------
   // | ___ |  ~  |PRTAB|NETAB|
   // -------------------------
-  [LAYER_SYMBOLS] = LAYOUT_ortho_4x4(
-    KC_AMPERSAND,     KC_ASTERISK,      KC_LPRN,     KC_KP_ASTERISK, \
-    KC_DOLLAR,        KC_PERCENT,       KC_CIRC,     KC_KP_SLASH, \
-    KC_EXCLAIM,       KC_AT,            KC_HASH,     CHANGE_WINDOW, \
-    LT(LAYER_SYMBOLS, KC_KP_DOT),KC_RPRN,PREV_TAB,NEXT_TAB
+  [LAYER_EMOJI] = LAYOUT_ortho_4x4(
+    EMOJI_SMILE, EMOJI_JOY,   EMOJI_TEAR,   KC_KP_PLUS,
+    EMOJI_RAISED_HANDS, EMOJI_INFO_DESK,   EMOJI_SPARKLE_THUMB,   EMOJI_DANGO,
+    EMOJI_THUMBS_DOWN, EMOJI_PEACE,   EMOJI_FIRE,   EMOJI_COOLGHOST,
+    EMOJI_THUMBS_UP, EMOJI_CRY_HAPPY, _NUMPAD, KC_ENTER
   ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case NEXT_TAB:
-      if (record->event.pressed) {
-        SEND_STRING(SS_DOWN(X_LGUI) SS_DOWN(X_LSHIFT) SS_TAP(X_RBRACKET) SS_UP(X_LGUI) SS_UP(X_LSHIFT));
-      }
-      break;
+    switch (keycode) {
+        case MAKE_CMD:
+            if (record->event.pressed) {
+              SEND_STRING("make thevankeyboards/roadkit:lstebner");
+            }
+            break;
 
-    case PREV_TAB:
-      if (record->event.pressed) {
-        SEND_STRING(SS_DOWN(X_LGUI) SS_DOWN(X_LSHIFT) SS_TAP(X_LBRACKET) SS_UP(X_LGUI) SS_UP(X_LSHIFT));
-      }
-      break;
+        case EMOJI_JOY:
+            if (record->event.pressed) {
+              SEND_STRING(":joy:");
+            }
+            break;
 
-    case NEW_TAB:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LGUI("t"));
-      }
-      break;
+        case EMOJI_SMILE:
+            if (record->event.pressed) {
+              SEND_STRING(":smile:");
+            }
+            break;
 
-    case NEW_WINDOW:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LGUI("n"));
-      }
-      break;
+        case EMOJI_TEAR:
+            if (record->event.pressed) {
+              SEND_STRING(":cry:");
+            }
+            break;
 
-    case COPY:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LGUI("c"));
-      }
-      break;
+        case EMOJI_RAISED_HANDS:
+            if (record->event.pressed) {
+              SEND_STRING(":raised_hands:");
+            }
+            break;
 
-    case PASTE:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LGUI("v"));
-      }
-      break;
+        case EMOJI_SPARKLE_THUMB:
+            if (record->event.pressed) {
+              SEND_STRING(":sparklethumb:");
+            }
+            break;
 
-    case CHANGE_WINDOW:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LGUI(SS_TAP(X_GRAVE)));
-      }
-      break;
+        case EMOJI_PEACE:
+            if (record->event.pressed) {
+              SEND_STRING(":peace:");
+            }
+            break;
 
-    case NEXT_APP:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LGUI(SS_TAP(X_TAB)));
-      }
-      break;
+        case EMOJI_FIRE:
+            if (record->event.pressed) {
+              SEND_STRING(":fire:");
+            }
+            break;
 
-    case PREV_APP:
-      if (record->event.pressed) {
-        SEND_STRING(SS_DOWN(X_LGUI) SS_DOWN(X_LSHIFT) SS_TAP(X_TAB) SS_UP(X_LGUI) SS_UP(X_LSHIFT));
-      }
-      break;
-        
-  }
+        case EMOJI_INFO_DESK:
+            if (record->event.pressed) {
+              SEND_STRING(":information_desk_person:");
+            }
+            break;
+
+        case EMOJI_THUMBS_UP:
+            if (record->event.pressed) {
+              SEND_STRING(":+1:");
+            }
+            break;
+
+        case EMOJI_THUMBS_DOWN:
+            if (record->event.pressed) {
+              SEND_STRING(":-1:");
+            }
+            break;
+
+        case EMOJI_CRY_HAPPY:
+            if (record->event.pressed) {
+              SEND_STRING(":cry_happy:");
+            }
+            break;
+
+        case EMOJI_DANGO:
+            if (record->event.pressed) {
+              SEND_STRING(":dango:");
+            }
+            break;
+
+        case EMOJI_TACO:
+            if (record->event.pressed) {
+              SEND_STRING(":taco:");
+            }
+            break;
+
+        case EMOJI_COOLGHOST:
+            if (record->event.pressed) {
+              SEND_STRING(":coolghost:");
+            }
+            break;
+
+        case EMOJI_PLUSONE:
+            if (record->event.pressed) {
+              SEND_STRING(":1:");
+            }
+            break;
+    }
   return true;
 };
 
